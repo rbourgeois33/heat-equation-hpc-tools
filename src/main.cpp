e0cf431468eafeecdd877a5973432d7d934e9760
remi.bourgeois@cea.fr <bourgeoisr@ruche01.cluster>
2023-11-16 11:36:12 +0100

    remove useless line

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 #include <mpi.h>
 #include <pdi.h>
 #include <Kokkos_Core.hpp>
-
 #include <iostream>
-
 
 int main(int argc, char** argv) 
 {
@@ -13,17 +11,17 @@ int main(int argc, char** argv)
 
   // Print Kokkos info
   Kokkos::print_configuration(std::cout);
-  
+
   //Get and print MPI values
   int mpi_rank, mpi_size;
   MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
   printf("Rank %d out of %d\n", mpi_rank, mpi_size);
-  fflush(stdout); // Flush the output buffer
 
   //Finalize Kokkos and MPI
   Kokkos::finalize();
   MPI_Finalize();
+
 
   return 0;
 }
