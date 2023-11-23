f1b8b8238d4f9ca44f13f285824a99138d5e167d
remi.bourgeois@cea.fr <bourgeoisr@ruche02.cluster>
2023-11-23 17:17:45 +0100

    jobs and readme

--- job_gpu.sh
+++ job_gpu.sh
@@ -10,7 +10,7 @@
 # To clean and load modules defined at the compile and link phases
 module purge
 module load gcc/11.2.0/gcc-4.8.5 hdf5/1.10.7/gcc-11.2.0-openmpi openmpi/4.1.1/gcc-11.2.0 cuda/11.7.0/gcc-11.2.0 cmake/3.21.4/gcc-11.2.0
-. /gpfs/users/bourgeoisr/expe_heat_equation/heat_equation/vendor/install_pdi/share/pdi/env.bash 
+. path_to_pdi_install/share/pdi/env.bash
 
 # echo of commands
 set -x
